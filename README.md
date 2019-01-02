755ce1a0d11a9a3dfdabc62df73cada3f978d4d1
Brandon Lamb <brlamb@starbucks.com>
2019-01-02 00:14:00 -0800

    messing with dyn4j

--- README.md
+++ README.md
@@ -13,5 +13,14 @@ The server will create `lobby`, `room-1` and `room-2` rooms.
 
 ## Architecture
 
+### Logic Flow
+
+This diagram shows how the game client would first connect to the authentication server, which would then provide a list of realms (a game instance, consisting of realm and world servers).
+
+Once authenticated against the auth server, the game client would connect to a realm, which would represent a game. From the realm server, the game client would then connect to a world server which would represent the actual map or level a player's character exists in.
+
+Given the SmartFoxServer 2X architecture, this could be either a single server running the realm (zone) and world (room) servers. Depending on scaling needs, you could have one server/VM/docker container running only a single world to scale out.
+
 ![Architecture Design Flow](architecturedesignflow.png)
+
 ![Architecture Design](architecturedesign.png)
--- demo1/src/main/kotlin/com/example/domain/core/PlayersActor.kt
+++ demo1/src/main/kotlin/com/example/domain/core/PlayersActor.kt
@@ -13,12 +13,22 @@ import com.example.domain.core.player.CheckPlayerConnected
 import com.example.domain.core.player.RemovePlayer
 import com.example.infrastructure.config.AppConfig
 import com.example.ports.sfs2x.RoomExtension
+import org.dyn4j.dynamics.Body
+import org.dyn4j.dynamics.BodyFixture
+import org.dyn4j.dynamics.World
+import org.dyn4j.geometry.MassType
+import org.dyn4j.geometry.Rectangle
+import org.dyn4j.geometry.Vector2
+import org.dyn4j.geometry.Vector3
 import org.pmw.tinylog.Logger
 import scala.concurrent.duration.Duration
+import java.awt.geom.Rectangle2D
 import java.util.concurrent.TimeUnit.MINUTES
 
 class PlayersActor(private val appConfig: AppConfig, private val room: RoomExtension) : AbstractActorWithTimers() {
   private object TickKey
+  private val world = World()
+  private var last = 0L
 
   override fun supervisorStrategy(): SupervisorStrategy = OneForOneStrategy(
     10,
@@ -31,6 +41,34 @@ class PlayersActor(private val appConfig: AppConfig, private val room: RoomExten
 
   init {
     Logger.info("Creating ${context.parent.path()}/${self.path().name()}")
+
+    // Init world
+    world.gravity = Vector2(0.0, 0.0)
+
+    for (i in 1..5) {
+      Body().apply {
+        addFixture(BodyFixture(Rectangle(10.0, 5.0)))
+        translate(10.0 * i, 10.0 * i)
+        setMassType(MassType.INFINITE)
+        world.addBody(this)
+      }
+
+      for (j in 1..5000) {
+        val time = System.nanoTime()
+        val delta = time - last
+        last = time
+
+        val elapsedTime = delta / NANO_TO_BASE
+
+        world.bodyIterator.forEach {
+          it.transform.translation.normalized
+        }
+
+        world.update(elapsedTime)
+      }
+    }
+
+
     timers.startPeriodicTimer(TickKey, CheckPlayerConnected, java.time.Duration.ofSeconds(CHECK_CONNECTED_TIMEOUT))
   }
 
@@ -89,6 +127,7 @@ class PlayersActor(private val appConfig: AppConfig, private val room: RoomExten
 
   companion object {
     private const val CHECK_CONNECTED_TIMEOUT = 10L
+    private const val NANO_TO_BASE = 1.0e9
 
     fun props(config: AppConfig, room: RoomExtension): Props = Props.create(PlayersActor::class.java, config, room)
   }
